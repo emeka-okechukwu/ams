2603f633018220eb61587794f577de6eff8a8ec2
emekaokechukwudev <43452059+emekaokechukwudev@users.noreply.github.com>
2022-03-04 10:33:20 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,14 +1,14 @@
 <?php
 //Development database connection
-define("DATABASE", "address_management_system");
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
+// define("DATABASE", "address_management_system");
+// define("SERVER", "localhost");
+// define("USERNAME", "root");
+// define("PASSWD", "");
 
 //Remote database credentials
-// define("DATABASE", "address_management_system");
-// define("SERVER", "http://sql11.freemysqlhosting.net/");
-// define("USERNAME", "sql11476656");
-// define("PASSWD", "CzIVBCbFmd");
+define("DATABASE", "address_management_system");
+define("SERVER", "http://sql11.freemysqlhosting.net/");
+define("USERNAME", "sql11476656");
+define("PASSWD", "CzIVBCbFmd");
 
 ?>
